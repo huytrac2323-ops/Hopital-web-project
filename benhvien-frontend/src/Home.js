0619f38d6c235696b7a7fa717ad8fc72610ba73c
NhatHuy <huytrac2323@gmail.com>
2026-08-25 21:09:00 +0700

    fix link, admin

--- benhvien-frontend/src/Home.js
+++ benhvien-frontend/src/Home.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './Home.css';
-import { Link } from 'react-router-dom';
 
 function Home() {
     const navigate = useNavigate();
